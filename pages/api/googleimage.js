6320ef96bac1217a0fdb19f7e33802eebe021973
itsreimau <private.iblmln@gmail.com>
2024-07-12 15:34:58 +0700

    fix

---
+++ pages/api/googleimage.js
@@ -0,0 +1,31 @@
+import {
+    googleImage
+} from "@bochilteam/scraper";
+
+const creator = "@itsreimau";
+
+export default async function handler(req, res) {
+    const {
+        search
+    } = req.query;
+
+    try {
+        if (!message) throw new Error("Parameter 'search' is required");
+
+        const result = await googleImage(search);
+
+        const json = {
+            status: true,
+            creator: creator,
+            result
+        };
+        res.status(200).json(json);
+    } catch (error) {
+        const json = {
+            status: false,
+            creator: creator,
+            error: error.message
+        };
+        res.status(400).json(json);
+    }
+}
